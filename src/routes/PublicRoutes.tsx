c8043c7d77be7f36355b975d11c1d45aab93e407
Dominic03144 <dominic.kosgei22@gmail.com>
2025-07-31 10:59:49 +0300

    navbar

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,7 +1,7 @@
-// ✅ src/routes/PublicRoutes.tsx
 import { Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
+// Pages that use MainLayout (with Navbar/Footer)
 import LandingPage from "../pages/public/HomePage";
 import HotelsPage from "../pages/usePages/HotelsPage";
 import HotelDetailsPage from "../pages/usePages/HotelDetailsPage";
@@ -10,26 +10,33 @@ import RoomDetailsPage from "../pages/usePages/RoomsDetailsPage";
 import ServicesPage from "../pages/public/ServicesPage";
 import AboutPage from "../pages/public/AboutPage";
 import ContactPage from "../pages/public/ContactPage";
+import PaymentSuccessPage from "../pages/PaymentSuccessPage";
+
+// Pages that should NOT use MainLayout (no Navbar/Footer)
 import RegisterPage from "../pages/public/RegisterPage";
 import LoginPage from "../pages/public/LoginPage";
 import VerifyEmailPage from "../pages/public/verifyEmailpage";
-import PaymentSuccessPage from "../pages/PaymentSuccessPage";
 
 const PublicRoutes = (
-  <Route element={<MainLayout />}>
-    <Route index element={<LandingPage />} />
-    <Route path="/hotels" element={<HotelsPage />} />
-    <Route path="/hotels/:id" element={<HotelDetailsPage />} />
-    <Route path="/hotels/:hotelId/rooms" element={<HotelRoomsPage />} />
-    <Route path="/rooms/:roomId" element={<RoomDetailsPage />} />
-    <Route path="/services" element={<ServicesPage />} />
-    <Route path="/about" element={<AboutPage />} />
-    <Route path="/contact" element={<ContactPage />} />
+  <>
+    {/* Routes with MainLayout */}
+    <Route element={<MainLayout />}>
+      <Route index element={<LandingPage />} />
+      <Route path="/hotels" element={<HotelsPage />} />
+      <Route path="/hotels/:id" element={<HotelDetailsPage />} />
+      <Route path="/hotels/:hotelId/rooms" element={<HotelRoomsPage />} />
+      <Route path="/rooms/:roomId" element={<RoomDetailsPage />} />
+      <Route path="/services" element={<ServicesPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/payment-success" element={<PaymentSuccessPage />} />
+    </Route>
+
+    {/* Routes without MainLayout (no duplicate navbar) */}
     <Route path="/register" element={<RegisterPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/verify-email" element={<VerifyEmailPage />} />
-    <Route path="/payment-success" element={<PaymentSuccessPage />} />
-  </Route>
+  </>
 );
 
 export default PublicRoutes;
